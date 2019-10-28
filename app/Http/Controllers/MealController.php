1cf8932adfa56419087a36b8578b418b5874754a
Luka <uskovicluka@gmail.com>
2019-10-28 18:45:12 +0100

    tag filter set to AND

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -19,29 +19,37 @@ class MealController extends Controller
 
         $meals = Meal::query();
 
-        if (isset($request->category)){
+        if (isset($request->category)) {
             $meals->whereIn('categoryId', $this->categoryFilter($request));
         }
 
-        if (isset($request->tags)){
+        if (isset($request->tags)) {
             $tags = array_map('intval', explode(',', $request->tags));
-            $meals->whereHas('tags',function ($q) use($tags) {
-                $q->whereIn('id', $tags);
-            });
+            foreach ($tags as $tag) {
+                $meals->whereHas('tags', function ($q) use ($tag) {
+                    $q->where('id', $tag);
+                });
+            }
         }
 
-        if (isset($request->with)){
-            $with =  array_map('strval', explode(',', $request->with));
-            if (in_array('tags', $with)) $meals->with('tags');
-            if (in_array('ingredients', $with)) $meals->with('ingredients');
-            if (in_array('category', $with)) $meals->with('category');
+        if (isset($request->with)) {
+            $with = array_map('strval', explode(',', $request->with));
+            if (in_array('tags', $with)) {
+                $meals->with('tags');
+            }
+            if (in_array('ingredients', $with)) {
+                $meals->with('ingredients');
+            }
+            if (in_array('category', $with)) {
+                $meals->with('category');
+            }
         }
 
-        if (isset($request->diff_time) & $request->diff_time>0 ){
+        if (isset($request->diff_time) & $request->diff_time > 0) {
             $meals->withTrashed();
         }
 
-        return  MealResource::collection($meals->simplePaginate($request->perPage));
+        return MealResource::collection($meals->simplePaginate($request->perPage));
     }
 
     public function categoryFilter(Request $request)
@@ -50,7 +58,7 @@ class MealController extends Controller
             return null;
         } elseif (strtolower($request->category) == '!null') {
             return !null;
-        } else{
+        } else {
             return array_map('intval', explode(',', $request->category));
         }
     }
